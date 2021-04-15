8dbf89f80cebbf9e251d4bd52db25a681abf5213
Abhishek Aggarwal <abhi@abhitopia.com>
2021-04-15 14:16:38 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='typegenie',
-    version='0.0.7',
+    version='0.0.8',
     url="https://github.com/abhitopia/TypeGenieApiClient",
     author="abhitopia",
     author_email="hi@typegenie.net",
