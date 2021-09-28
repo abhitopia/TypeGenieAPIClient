02dc08142591e864b0118013ec5ef0f072664413
Renato Rocha <renato@typegenie.net>
2021-09-28 09:47:09 +0100

    Correctly bumped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ src_dir = str(Path(__file__).parent)
 
 setup(
     name='typegenie',
-    version='0.2.6',
+    version='0.2.0',
     url="https://github.com/abhitopia/TypeGenieApiClient",
     author="abhitopia",
     author_email="hi@typegenie.net",
