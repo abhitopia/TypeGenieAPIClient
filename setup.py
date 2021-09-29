03173d9e47402e79c62be76f6b6423c440fedee0
Renato Rocha <renato@typegenie.net>
2021-09-29 11:22:01 +0100

    Artificial version bump for testing purposes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ src_dir = str(Path(__file__).parent)
 
 setup(
     name='typegenie',
-    version='0.1.6',
+    version='0.2.1',
     url="https://github.com/abhitopia/TypeGenieApiClient",
     author="abhitopia",
     author_email="hi@typegenie.net",
